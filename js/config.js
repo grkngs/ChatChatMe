63a72771d9c23d6d743a2e2f149ca3f2cba2a499
grkngs <56035587+grkngs@users.noreply.github.com>
2021-12-19 17:58:46 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ const config = {
   messagingSenderId: "593160946673",
   appId: "1:593160946673:web:3dde532e989705590d3a50",
 databaseUrl: "https://javascript-chat-app-c3312-default-rtdb.europe-west1.firebasedatabase.app/",
-  CometChatAppId: "c400584767ca76df59e852b30f645231f6cd21b4",
+  CometChatAppId: "19976335bcec6f80",
   CometChatRegion: "eu",
   CometChatAuthKey: "c400584767ca76df59e852b30f645231f6cd21b4",
   CometChatAPIKey: "80c5b303c89be0795d10f3736e9a36c940409f19",
