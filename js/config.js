d2b11b6e0e514d8e596b6414fdea97d7367ca92b
grkngs <56035587+grkngs@users.noreply.github.com>
2021-12-19 21:09:55 +0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,14 @@
+const config = {
+  apiKey: "AIzaSyDOIyiVY2JKHKZglrMd2J5nRsXPD1Vo98U",
+  authDomain: "javascript-chat-app-c3312.firebaseapp.com",
+  databaseURL: "https://javascript-chat-app-c3312-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "javascript-chat-app-c3312",
+  storageBucket: "javascript-chat-app-c3312.appspot.com",
+  messagingSenderId: "593160946673",
+  appId: "1:593160946673:web:5c301e53757de8840d3a50",
+  CometChatAppId: "199776ca8706f27c",
+  CometChatRegion: "eu",
+  CometChatAuthKey: "4cd1efa223ad4689c06c7202a1b2b3d2a0a22004",
+  CometChatAPIKey: "3f8e5da14c405c6d0275aa1d39a2d49be566092c",
+  CometChatWidgetId: "a5498c6e-5c6a-4fdc-a34b-a00883fbbafd"
+};
